7bf55d59a969fb7db88ebec4b01f0d37aacdd9d4
humeng <humeng_mail@163.com>
2023-09-04 14:47:03 +0800

    update: 测试qps

---
+++ rocketmq-demo04-tomcat-qps-test/src/main/java/com/hm/RocketmqDemo04TomcatQpsTestApplication.java
@@ -0,0 +1,13 @@
+package com.hm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqDemo04TomcatQpsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketmqDemo04TomcatQpsTestApplication.class, args);
+	}
+
+}
